96bee012c804e7f32c1abb1030c7a851a0f6abde
Felipe Villegas <felavid@gmail.com>
2021-07-09 23:36:41 -0400

    Game fully functional; play button added

--- settings.py
+++ settings.py
@@ -1,20 +1,27 @@
 class Settings:
-  """A class to store all settings for Alien Invasion."""
+    """A class to store all settings for Alien Invasion."""
 
-  def __init__(self):
-      """Initialize the game's settings"""
-      # Screen settings
-      self.screen_width = 1200
-      self.screen_height = 800
-      self.bg_color = (230, 230, 230)
-      self.fullscreen = False
+    def __init__(self):
+        """Initialize the game's settings"""
+        # Screen settings
+        self.screen_width = 1200            # Default: 1200
+        self.screen_height = 800            # Default: 800
+        self.bg_color = (230, 230, 230)     # Default: (230, 230, 230)
+        self.fullscreen = False             # Default: False
 
-      # Ship settings
-      self.ship_speed = 1.5
+        # Ship settings
+        self.ship_speed = 1.5               # Default: 1.5
+        self.ship_limit = 3                 # Default: 3
 
-      # Bullet settings
-      self.bullet_speed = 1.0
-      self.bullet_width = 3
-      self.bullet_height = 15
-      self.bullet_color = (60, 60, 60)
-      self.bullets_allowed = 3
+        # Bullet settings
+        self.bullet_speed = 1.0             # Default: 1.0
+        self.bullet_width = 3               # Default: 3
+        self.bullet_height = 15             # Default: 15
+        self.bullet_color = (60, 60, 60)    # Default: (60, 60, 60)
+        self.bullets_allowed = 3            # Default: 3
+
+        # Alien settings
+        self.alien_speed = 1.0              # Default: 1.0
+        self.fleet_drop_speed = 10          # Default: 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1            # Default: 1
